dc89a0227f85372c8b38d5b82a7b4164b8714287
PhilLint <PhilLint@users.noreply.github.com>
2020-01-30 11:50:01 +0100

    bias experiment

--- find_and_remove_duplicates.py
+++ find_and_remove_duplicates.py
@@ -9,48 +9,59 @@ import imageio
 
 path = os.getcwd()
 
-nurses_male = path + "/nurses/nurse_male"
-nurses_female = path + "/nurses/nurse_female"
-doctors_male = path + "/doctors/doctor_male"
-doctors_female= path + "/doctors/doctor_female"
-
-os.chdir(doctors_female)
-
-file_list = os.listdir()
-print(len(file_list))
-
 def file_hash(filepath):
     with open(filepath, 'rb') as f:
         return hashlib.md5(f.read()).hexdigest()
 
-duplicates = []
-hash_keys = dict()
-for index, filename in  enumerate(os.listdir('.')):  #listdir('.') = current directory
-    if os.path.isfile(filename):
-        with open(filename, 'rb') as f:
-            filehash = hashlib.md5(f.read()).hexdigest()
-        if filehash not in hash_keys:
-            hash_keys[filehash] = index
-        else:
-            duplicates.append((index,hash_keys[filehash]))
+def remove_duplicates(image_path):
+    duplicates = []
+    hash_keys = dict()
 
-print(len(duplicates))
+    os.chdir(image_path)
+    file_list = os.listdir()
+    print("Number of files: {}".format(len(file_list)))
+
+    for index, filename in  enumerate(os.listdir('.')):  #listdir('.') = current directory
+        if os.path.isfile(filename):
+            with open(filename, 'rb') as f:
+                filehash = hashlib.md5(f.read()).hexdigest()
+            if filehash not in hash_keys:
+                hash_keys[filehash] = index
+            else:
+                duplicates.append((index,hash_keys[filehash]))
+
+    print("Number of duplicates: {}".format(len(duplicates)))
+    # remove duplicates
+    for index in duplicates:
+        os.remove(file_list[index[0]])
+    os.chdir(path)
+
+
+
+nurses_male = path + "/biased_data/nurses/nurse_male"
+nurses_female = path + "/biased_data/nurses/nurse_female"
+doctors_male = path + "/biased_data/doctors/doctor_male"
+doctors_female= path + "/biased_data/doctors/doctor_female"
+
+remove_duplicates(nurses_male)
+remove_duplicates(nurses_female)
+remove_duplicates(doctors_male)
+remove_duplicates(doctors_female)
 
 # visualize duplicates
-for file_indexes in duplicates[:30]:
-    try:
+# for file_indexes in duplicates[:10]:
+#     try:
+#
+#         plt.subplot(121), plt.imshow(imageio.imread(file_list[file_indexes[1]]))
+#         plt.title(file_indexes[1]), plt.xticks([]), plt.yticks([])
+#
+#         plt.subplot(122), plt.imshow(imageio.imread(file_list[file_indexes[0]]))
+#         plt.title(str(file_indexes[0]) + ' duplicate'), plt.xticks([]), plt.yticks([])
+#         plt.show()
+#
+#     except OSError as e:
+#         continue
 
-        plt.subplot(121), plt.imshow(imageio.imread(file_list[file_indexes[1]]))
-        plt.title(file_indexes[1]), plt.xticks([]), plt.yticks([])
 
-        plt.subplot(122), plt.imshow(imageio.imread(file_list[file_indexes[0]]))
-        plt.title(str(file_indexes[0]) + ' duplicate'), plt.xticks([]), plt.yticks([])
-        plt.show()
-
-    except OSError as e:
-        continue
-# remove duplicates
-for index in duplicates:
-    os.remove(file_list[index[0]])
 
 
