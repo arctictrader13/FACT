82c67a5aa553e9da75a53769180c6bd45d3078a9
Anca-Diana <ancadianavicol@gmail.com>
2020-01-31 23:32:18 +0100

    notebook

--- remove_and_retrain.py
+++ remove_and_retrain.py
@@ -20,13 +20,13 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
 
-def train(model, data_loader=None, data_path="", plot_name=""):
+def train(model, config, data_loader=None, data_path="", plot_name=""):
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.SGD(model.classifier.parameters(),
-                                lr=ARGS.initial_learning_rate, momentum=0.9)
+                                lr=config['initial_learning_rate'], momentum=0.9)
     scheduler = lr_scheduler.StepLR(optimizer,
-                                    step_size=ARGS.lr_decresing_step,
-                                    gamma=ARGS.lr_gamma)
+                                    step_size=config['lr_decresing_step'],
+                                    gamma=config['lr_gamma'])
     model.train()
 
     if data_loader != None:
@@ -35,10 +35,10 @@ def train(model, data_loader=None, data_path="", plot_name=""):
     losses = []
     loss_steps = []
     num_batches = 0.0
-    for _ in range(ARGS.epochs):
+    for _ in range(config['epochs']):
         accuracy = 0.0
         step = 0
-        while step != ARGS.max_train_steps:
+        while step != config['max_train_steps']:
             if data_path != "":
                 if not os.path.exists(os.path.join(data_path,
                                                    "batch_input" + str(step))):
@@ -53,8 +53,8 @@ def train(model, data_loader=None, data_path="", plot_name=""):
                 except StopIteration:
                     data_iterator = iter(data_loader)
                     break
-            batch_inputs = batch_inputs.to(ARGS.device)
-            batch_targets = batch_targets.to(ARGS.device)
+            batch_inputs = batch_inputs.to(config['device'])
+            batch_targets = batch_targets.to(config['device'])
 
             num_batches += 1
 
@@ -67,17 +67,17 @@ def train(model, data_loader=None, data_path="", plot_name=""):
             loss.backward()
             optimizer.step()
 
-            if step % ARGS.print_step == 0 and step != 0:
+            if step % config['print_step'] == 0 and step != 0:
                 losses += [float(loss)]
                 loss_steps += [num_batches]
-                accuracy = float(accuracy) / float(ARGS.batch_size * ARGS.print_step)
+                accuracy = float(accuracy) / float(config['batch_size'] * config['print_step'])
 
                 if (len(losses) > 2 and abs(losses[-1] - losses[-2]) < 0.0001):
                     break
                 print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, "
                       "Accuracy = {:.2f}, Train Loss = {:.3f}".format(
                           datetime.now().strftime("%Y-%m-%d %H:%M"), step,
-                          0, ARGS.batch_size, accuracy, loss
+                          0, config['batch_size'], accuracy, loss
                           ))
                 accuracy = 0.0
             step += 1
@@ -101,7 +101,7 @@ def train(model, data_loader=None, data_path="", plot_name=""):
         json.dump({"steps": loss_steps, "loss": losses}, f)
 
 
-def test(model, data_loader=None, data_path=""):
+def test(model, config, data_loader=None, data_path=""):
     model.eval()
     accuracies = []
     step = 0
@@ -109,7 +109,7 @@ def test(model, data_loader=None, data_path=""):
     if data_loader != None:
         data_iterator = iter(data_loader)
 
-    while step != ARGS.max_train_steps:
+    while step != config['max_train_steps']:
         if data_path != "":
             if not os.path.exists(os.path.join(data_path,
                                                "batch_input" + str(step))):
@@ -123,26 +123,26 @@ def test(model, data_loader=None, data_path=""):
                 batch_inputs, batch_targets = next(data_iterator)
             except StopIteration:
                 break
-        batch_inputs = batch_inputs.to(ARGS.device)
-        batch_targets = batch_targets.to(ARGS.device)
+        batch_inputs = batch_inputs.to(config['device'])
+        batch_targets = batch_targets.to(config['device'])
         batch_inputs.requires_grad = False
 
         output = model.forward(batch_inputs)
         accuracies += [float(sum(batch_targets ==
-                                 torch.argmax(output, 1))) / ARGS.batch_size]
+                                 torch.argmax(output, 1))) / config['batch_size']]
         step += 1
     accuracies = torch.tensor(accuracies)
     return float(accuracies.mean()), float(accuracies.std())
 
 
-def init_model():
-    model = vgg11(pretrained=True, in_size=32).to(ARGS.device)
+def init_model(config):
+    model = vgg11(pretrained=True, in_size=32).to(config['device'])
     for param in  model.features:
         param.requires_grad = False
 
-    model.classifier[0] = torch.nn.Linear(in_features=512, out_features=256, bias=True).to(ARGS.device)
-    model.classifier[3] = torch.nn.Linear(in_features=256, out_features=128, bias=True).to(ARGS.device)
-    model.classifier[6] = torch.nn.Linear(in_features=128, out_features=10, bias=True).to(ARGS.device)
+    model.classifier[0] = torch.nn.Linear(in_features=512, out_features=256, bias=True).to(config['device'])
+    model.classifier[3] = torch.nn.Linear(in_features=256, out_features=128, bias=True).to(config['device'])
+    model.classifier[6] = torch.nn.Linear(in_features=128, out_features=10, bias=True).to(config['device'])
 
     return model
 
@@ -161,33 +161,33 @@ def get_saliency_methods(grad_names, initial_model):
     return saliency_methods
 
 
-def remove_and_retrain(data_path):
+def remove_and_retrain(data_path, config, addition=""):
     initial_model = torch.load(os.path.join("results", "remove_and_retrain", "models", "initial_model"))
-    saliency_methods = get_saliency_methods(ARGS.grads, initial_model)
+    saliency_methods = get_saliency_methods(config['grads'], initial_model)
 
     colors = {"FullGrad": "#0074d9", "gradcam": "#111111",
               "Random": "#f012be", "Input-Gradient": "#01ff70"}
-    total_features = ARGS.img_size * ARGS.img_size
-    accuracies_mean = [[0.0 for _ in range(len(ARGS.k))] for _ in range(len(saliency_methods))]
-    accuracies_std = [[0.0 for _ in range(len(ARGS.k))] for _ in range(len(saliency_methods))]
+    total_features = config['img_size'] * config['img_size']
+    accuracies_mean = [[0.0 for _ in range(len(config['k']))] for _ in range(len(saliency_methods))]
+    accuracies_std = [[0.0 for _ in range(len(config['k']))] for _ in range(len(saliency_methods))]
     for method_idx, (_, method_name) in enumerate(saliency_methods):
         modified_data_path = os.path.join(data_path, "modified_cifar_10", method_name)
 
-        for k_idx, k in enumerate(ARGS.k):
+        for k_idx, k in enumerate(config['k']):
             print("Run saliency method: ", method_name)
-            data_path = os.path.join(modified_data_path, str(int(k * total_features)))
-
-            model = init_model()
-            train(model, data_path=os.path.join(data_path, "train"),
+            train_data_path = os.path.join(modified_data_path, str(int(k * total_features)))
+            print(train_data_path)
+            model = init_model(config)
+            train(model, config, data_path=os.path.join(train_data_path, "train"),
                   plot_name=method_name + "_" + str(k))
             accuracies_mean[method_idx][k_idx], accuracies_std[method_idx][k_idx] \
-             = test(model, data_path=os.path.join(data_path, "test"))
+             = test(model, config, data_path=os.path.join(train_data_path, "test"))
 
         plt.figure(0)
-        print([k * 100 for k in ARGS.k])
+        print([k * 100 for k in config['k']])
         print(accuracies_mean[method_idx])
         print(accuracies_std[method_idx])
-        plt.errorbar([k * 100 for k in ARGS.k], accuracies_mean[method_idx],
+        plt.errorbar([k * 100 for k in config['k']], accuracies_mean[method_idx],
                      accuracies_std[method_idx], label=method_name,
                      color=colors[method_name])
 
@@ -195,26 +195,26 @@ def remove_and_retrain(data_path):
     plt.ylabel('Accuracy')
     plt.xlabel('k %')
     plt.legend()
-    plt.savefig(os.path.join("results", "remove_and_retrain", "final_plot.jpeg"))
+    plt.savefig(os.path.join("results", "remove_and_retrain", addition + "final_plot.jpeg"))
 
     with open(os.path.join("results", "remove_and_retrain",
-                           "final_output" + ".json"), 'w') as f:
+                           addition + "final_output" + ".json"), 'w') as f:
         json.dump({"mean": accuracies_mean, "std": accuracies_std}, f)
 
 
-def compute_modified_datasets(train_set_loader, test_set_loader, data_path):
+def compute_modified_datasets(train_set_loader, test_set_loader, data_path, config):
     initial_model = torch.load(os.path.join("results", "remove_and_retrain",
                                             "models", "initial_model"))
-    saliency_methods = get_saliency_methods(ARGS.grads, initial_model)
+    saliency_methods = get_saliency_methods(config['grads'], initial_model)
 
-    total_features = ARGS.img_size * ARGS.img_size
+    total_features = config['img_size'] * config['img_size']
     for _, (saliency_method, method_name) in enumerate(saliency_methods):
         for dataset, dataloader in [("train", train_set_loader), ("test", test_set_loader)]:
             saliency_path = os.path.join(data_path, "saliency_maps", method_name + "_vgg11", dataset)
             if saliency_method != None:
                 misc_functions.compute_and_store_saliency_maps(dataloader, initial_model, \
-                    ARGS.device, ARGS.max_train_steps, saliency_method, saliency_path)
-            for k in ARGS.k:
+                    config['device'], config['max_train_steps'], saliency_method, saliency_path)
+            for k in config['k']:
                 num_pixels = int(k * total_features)
 
                 dataset_path = os.path.join(data_path, "modified_cifar_10", method_name, str(num_pixels))
@@ -227,17 +227,17 @@ def compute_modified_datasets(train_set_loader, test_set_loader, data_path):
                         saliency_map = torch.load(os.path.join(saliency_path, \
                             "saliency_map_" + str(step)))
                         data = misc_functions.remove_salient_pixels(batch_inputs, saliency_map, \
-                            num_pixels=num_pixels, most_salient=ARGS.most_salient)
+                            num_pixels=num_pixels, most_salient=config['most_salient'])
                     torch.save(data, os.path.join(dataset_path, dataset, "batch_input" + str(step)))
                     torch.save(batch_targets, os.path.join(dataset_path, dataset, "batch_target" + str(step)))
 
-                    if step == ARGS.max_train_steps:
+                    if step == config['max_train_steps']:
                         break
 
 
-def load_cifar_dataset(data_path):
+def load_cifar_dataset(data_path, config):
     transform_standard = transforms.Compose([
-        transforms.Resize((ARGS.img_size, ARGS.img_size)),
+        transforms.Resize((config['img_size'], config['img_size'])),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225]), ])
@@ -246,30 +246,31 @@ def load_cifar_dataset(data_path):
         target_transform=None, download=True)
     test_set = datasets.CIFAR10(cifar_path, train=False, transform=transform_standard, \
         target_transform=None, download=True)
-    train_set_loader = torch.utils.data.DataLoader(train_set, batch_size=ARGS.batch_size, shuffle=False)
-    test_set_loader = torch.utils.data.DataLoader(test_set, batch_size=ARGS.batch_size, shuffle=False)
+    train_set_loader = torch.utils.data.DataLoader(train_set, batch_size=config['batch_size'], shuffle=False)
+    test_set_loader = torch.utils.data.DataLoader(test_set, batch_size=config['batch_size'], shuffle=False)
 
     return train_set_loader, test_set_loader
 
 
 def main():
-    data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), ARGS.data_directory)
+    config = vars(ARGS)
+    data_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), config['data_directory'])
 
-    if ARGS.phase == "train_initial_model":
-        train_set_loader, test_set_loader = load_cifar_dataset(data_path)
-        initial_model = init_model()
+    if config['phase'] == "train_initial_model":
+        train_set_loader, test_set_loader = load_cifar_dataset(data_path, config)
+        initial_model = init_model(config)
 
-        train(initial_model, data_loader=train_set_loader, plot_name="initial_model")
+        train(initial_model, config, data_loader=train_set_loader, plot_name="initial_model")
 
-        initial_accuracy_mean, initial_accuracy_std = test(initial_model, data_loader=test_set_loader)
+        initial_accuracy_mean, initial_accuracy_std = test(initial_model, config, data_loader=test_set_loader)
         print(initial_accuracy_mean, initial_accuracy_std)
 
-    elif ARGS.phase == "create_modified_datasets":
-        train_set_loader, test_set_loader = load_cifar_dataset(data_path)
-        compute_modified_datasets(train_set_loader, test_set_loader, data_path)
+    elif config['phase'] == "create_modified_datasets":
+        train_set_loader, test_set_loader = load_cifar_dataset(data_path, config)
+        compute_modified_datasets(train_set_loader, test_set_loader, data_path, config)
 
-    elif ARGS.phase == "train_on_modified_datasets":
-        remove_and_retrain(data_path)
+    elif config['phase'] == "train_on_modified_datasets":
+        remove_and_retrain(data_path, config)
 
 
 if __name__ == "__main__":
@@ -314,4 +315,5 @@ if __name__ == "__main__":
 
 
     ARGS = parser.parse_args()
+
     main()
